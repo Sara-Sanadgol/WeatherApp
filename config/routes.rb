42f7144e55dfc10f62ed169b86cefa86408ce937
sara <sara_sanadgol@yahoo.com>
2018-11-24 00:17:53 -0500

    webpack added for react

--- config/routes.rb
+++ config/routes.rb
@@ -7,7 +7,7 @@ Rails.application.routes.draw do
   get 'weathers/update'
   get 'weathers/destroy'
 
-  root to: "home#index"
+  root to: "static#root"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 resources :weathers
 
